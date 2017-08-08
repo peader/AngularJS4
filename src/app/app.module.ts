0ae6ab6017f5a338706b111f0eb23cbc2d0a77e9
PeterM <peter.mahady1@gmail.com>
2017-08-08 20:45:26 +0200

    Start to add the component structure for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppinglisteditComponent } from './shopp-list/shoppinglistedit/shoppinglistedit.component';
+import { RecipelistComponent } from './recipes/recipelist/recipelist.component';
+import { RecipeitemComponent } from './recipes/recipelist/recipeitem/recipeitem.component';
+import { RecipedetailComponent } from './recipes/recipedetail/recipedetail.component';
+import {HeaderComponent} from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppListComponent } from './shopp-list/shopp-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppinglisteditComponent,
+    RecipelistComponent,
+    RecipeitemComponent,
+    RecipedetailComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopp-list/shopp-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopp-list',
+  templateUrl: './shopp-list.component.html',
+  styleUrls: ['./shopp-list.component.css']
+})
+export class ShoppListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
