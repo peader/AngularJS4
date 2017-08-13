1458e24487692ddbed1b92d0ce60092322fef232
PeterM <peter.mahady1@gmail.com>
2017-08-13 22:05:05 +0200

    Add the header and recipe list html. Also start to implement the recipe model

--- src/app/recipes/recipelist/recipelist.component.ts
+++ src/app/recipes/recipelist/recipelist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipelist',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RecipelistComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'this is simply a test', 'https://www.google.de/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwixyeXD_tTVAhWCmbQKHXfuBSYQjRwIBw&url=http%3A%2F%2Fallrecipes.com%2Frecipes%2F84%2Fhealthy-recipes%2F&psig=AFQjCNGuS0s0gaQ0tpARavMzb_AyOK1M1A&ust=1502740433295987')
+  ];
   constructor() { }
 
   ngOnInit() {
